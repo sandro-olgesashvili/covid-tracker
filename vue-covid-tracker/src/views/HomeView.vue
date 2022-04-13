759d44bb6793daa5711b488a53ebff20f597b9dc
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-04-13 20:17:32 +0400

    add fetch

--- vue-covid-tracker/src/views/HomeView.vue
+++ vue-covid-tracker/src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
+  <main v-if="!loading">
+
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
     
-  </div>
+  </main>
 </template>
 
 <script>
@@ -11,6 +19,41 @@ export default {
   name: 'HomeView',
   components: {
     
+  },
+
+  data() {
+    return {
+      loading:true,
+      title:'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+
+    }
+  },
+
+
+  methods:{
+    async fetchCovidDate() {
+      const res =  await fetch('https://api.covid19api.com/summary');
+      const data = await res.json ();
+      return data
+    }
+  },
+
+
+  async created() {
+    const data = await this.fetchCovidDate();
+    
+    console.log(data)
+
+
+    this.dataDate = data.Date
+    this.countries = data.Countries
+    this.stats = data.Global
+    this.loading= false
+
   }
 }
 </script>

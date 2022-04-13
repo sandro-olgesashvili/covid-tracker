9a4548c5cdd91a2be593ddfbcf15d442702e85b0
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-04-13 19:46:25 +0400

    add Header

---
+++ vue-covid-tracker/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10"  >
+      <div class="text-3xl md:text-5xl mb-3 font-bold uppercase"> <i class="fa fa-viruses"></i> covid tracker</div>
+      <p>API by <a href="https://covid19api.com" class="text-blue-300" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue-covid-tracker/tailwind.config.js
+++ vue-covid-tracker/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    container:{
+      center:true,
+      padding: '2rem'
+    }
   },
   plugins: [],
 }

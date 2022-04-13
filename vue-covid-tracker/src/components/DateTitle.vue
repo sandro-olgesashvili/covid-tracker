2d6948c126fe4d73400c07c947c25eef88a61937
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-04-13 23:09:24 +0400

    done

--- vue-covid-tracker/src/components/DateTitle.vue
+++ vue-covid-tracker/src/components/DateTitle.vue
@@ -14,7 +14,6 @@ import moment from 'moment'
 export default {
     name: "DataTitle",
     props: ['text', 'dataDate'],
-
     computed: {
         timestamp: function() {
           return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
